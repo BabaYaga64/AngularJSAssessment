24db9e7346f540e38f5e275f75e5670f3bb00afd
Bojana Skarich <bojana.skarich@gmail.com>
2015-04-24 13:58:50 -0700

    Add controller and factory files. Add home state and other two state files.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var signIn = angular.module('signIn', ['ui.router']);
+
+//Renders home page state
+signIn.config(function($stateProvider) {
+	$stateProvider.state('home', {
+		url: "",
+		templateUrl: 'partials/home.html'
+	});
+
+});
+
+
