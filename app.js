fc8d9327ec5f4221528d85e42df6ccbdc6e5cfc9
Bojana Skarich <bojana.skarich@gmail.com>
2015-04-24 14:27:57 -0700

    Update app.js to have three states.

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ var signIn = angular.module('signIn', ['ui.router']);
 signIn.config(function($stateProvider) {
 	$stateProvider.state('home', {
 		url: "",
-		templateUrl: 'partials/home.html'
+		templateUrl: 'partials/home.html',
+		controller: 'StudentCtrl'
+	});
+
+stateProvider.state('signin', {
+	url: "signin",
+	templateUrl: 'partials/signin.html',
+	controller: 'StudentCtrl'
+
+	});
+
+stateProvider.state('whosthere', {
+	url: "whosethere",
+	templateUrl: 'partials/whosethere.html',
+	controller: 'StudentCtrl'
+
 	});
 
 });
