1950c2baf4a62c9752fed0d21e4ee68d343571c0
Bojana Skarich <bojana.skarich@gmail.com>
2015-04-24 15:06:46 -0700

    Add deleteStudent and addStudent methods to StudentCtrl.

--- controllers/StudentCtrl.js
+++ controllers/StudentCtrl.js
@@ -2,11 +2,22 @@ signIn.controller('StudentCtrl', function StudentCtrl($scope, StudentFactory) {
 	$scope.students = StudentFactory.students;
 	$scope.StudentFactory = StudentFactory;
 
+	$scope.addStudent = function() {
+		var name = student.studentName;
+		StudentFactory.addStudent(name);
+		$scope.studentName = null;
+	};
+
+	$scope.deleteStudent = function() {
+		var name = $scope.studentName;
+		StudentFactory.deleteStudent(name);
+	}; 
+
 	$scope.signIn = function(student) {
 		student.attendence = true;
-	}
+	};
 
 	$scope.signOut = function(student) {
 		student.attendence = false;
-	}
+	};
 });
